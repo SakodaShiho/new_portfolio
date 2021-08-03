7462e3c58c1623af2a1f7a5ebe0b52b0aa401e95
“SakodaShiho” <sawadasakura322@gmail.com>
2021-08-03 15:22:06 +0900

    career modal add

--- js/script.js
+++ js/script.js
@@ -70,3 +70,20 @@ $(
     });
   })()
 );
+
+// モーダル
+
+$(function () {
+  $('.js-open').click(function () {
+    $('body').addClass('no_scroll');
+
+    var id = $(this).data('id');
+    $('#overlay, .modal-window[data-id="modal' + id + '"]').fadeIn();
+  });
+
+  $('.js-close , #overlay').click(function () {
+    $('body').removeClass('no_scroll');
+
+    $('#overlay, .modal-window').fadeOut();
+  });
+});
